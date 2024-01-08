08e8cb8a832d0b1241a2120218c09c99cfc2c66d
ankugomkar13 <120354060+ankugomkar13@users.noreply.github.com>
2024-01-08 21:56:12 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,19 @@
+public class Evenodd
+{
+	public static void main(String[] args) {
+		int arr[]={2,4,0,56,78,23,0,1,2};
+	    
+	    for(int i=0;i<9;i++)
+	    {
+	        if(arr[i]%2==0)
+	        {
+	            System.out.println(arr[i]);
+	        }
+	  
+	        if(arr[i]%2!=0)
+	        {
+	            System.out.println(arr[i]);
+	        }
+	    }
+}
+}
