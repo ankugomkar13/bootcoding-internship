8c6b0d72d317bbe2f81393abc1bbc27f40531c8c
Ankita Yashwant Gomkar <120354060+ankugomkar13@users.noreply.github.com>
2024-01-09 13:51:40 +0530

    Add files via upload

---
+++ Equalarr.java
@@ -0,0 +1,17 @@
+mport java.util.Arrays;
+public class Equalarr
+{
+	public static void main(String[] args) {
+
+        int[] a1 = {1, 2, 3, 4, 5, 6, 7, 8};   
+        int[] a2 = {1, 2, 3, 4, 5, 6, 7, 8};   
+  
+        if (Arrays.equals(a1,a2))   
+           System.out.println("Arrays are equal.");   
+        else  
+           System.out.println("Arrays are not equal."); 
+		    
+		    
+		
+	}
+}  
